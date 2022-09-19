59b48ac139fbc69a8002cc60a297c8e7a3af5564
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-09-19 15:50:42 +0700

    Fix Bug

--- main.py
+++ main.py
@@ -1,70 +1,83 @@
-pilihan = "y"
-while pilihan == "y":
-    print("""
-    +-------------------------------+
-               KOPI KOPO
-    +-------------------------------+
-    A. ES Kopi Susu   : Rp 11.000,-
-    B. ES Kopi Coklat : Rp 12.000,-
-    C. Capuccino Panas: Rp 11.000,-
-    D. Ice Americano  : Rp 14.000,-
-    +-------------------------------+
-    Note :
-        Silahkan Dipesan Sesuai Ruls
-    """)
-    pesan = str(input("Input a,b,c Atau d Dibawah \n"
-                      "Silahkan Input : "))
-    jumlahpesan = int(input("Masukan Jumlah Pesanan : "))
-    if pesan == "a":
-        listnama = "ES Kopi Susu"
-        harga = (11000 * jumlahpesan)
-        ppn = int(harga * 0.1)
-        if jumlahpesan >= 5:
-            diskon = int(harga * 0.2)
-            totalharga = int(harga - diskon + ppn)
-        else:
-            diskon = (0)
-            totalharga = int(harga + ppn)
-    elif pesan == "b":
-        listnama = "ES Kopi Coklat"
-        harga = (12000 * jumlahpesan)
-        ppn = int(harga * 0.1)
-        if jumlahpesan >= 5:
-            diskon = int(harga * 0.2)
-            totalharga = int(harga - diskon + ppn)
-        else:
-            diskon = (0)
-            totalharga = int(harga + ppn)
-    elif pesan == "c":
-        listnama = "Capuccino Panas"
-        harga = int(11000 * jumlahpesan)
-        ppn = int(harga * 0.1)
-        diskon = 0
-        totalharga = int(harga + ppn)
-    elif pesan == "c":
-        listnama = "Ice Americano"
-        harga = int(14000 * jumlahpesan)
-        ppn = int(harga * 0.1)
-        diskon = 0
-        totalharga = int(harga + ppn)
-    else:
-        listnama = "-"
-        harga = "-"
-        ppn = "-"
-        diskon = "-"
-        totalharga = "-"
-        pilihan = input("Menu Tidak Tersedia Silahkan Kembali Dengan Menginputkan Y/N : ")
+print("----------------- Warung Programmer -----------------")
+pembeli = input("nama Pembeli: ")
+print("Nama Pembeli :", pembeli)
 
-        print("+-------------------------------+")
-        print("            KOPI KOPO\n"
-              "Jln Sadang No.43 Margahayu Tengah")
-        print("+-------------------------------+")
-        print("Menu           : ", listnama)
-        print("Jumlah Pesanan : ", jumlahpesan)
-        print("Harga          : ", harga)
-        print("Diskon         : ", diskon)
-        print("PPN            : ", ppn)
-        print("+-------------------------------+")
-        print("Jumlah Bayar   : ", totalharga)
-        print("+-------------------------------+")
-        pilihan = input("Bila Ingin Pesan Kembali Input Y/N : ")
+
+def fungsimakanan():
+    global totalmkn
+    global porsi
+    global mkn
+    print("\n----------------- Menu Makanan -----------------")
+    print("1. Nasi Goreng - Rp 15000")
+    print("2. Soto - Rp 9000")
+    print("3. Mie Ayam - Rp 11000")
+    nomor = int(input("Masukan Pilihan: "))
+    porsi = int(input("Berapa Porsi ?: "))
+
+    if nomor == 1:
+        totalmkn = porsi * 15000
+        print(porsi, " Porsi Nasi Goreng = Rp", totalmkn)
+        mkn = ("Nasi Goreng")
+    elif nomor == 2:
+        totalmkn = porsi * 9000
+        print(porsi, " Porsi Soto = Rp", totalmkn)
+        mkn = ("Soto")
+    elif nomor == 3:
+        totalmkn = porsi * 11000
+        print(porsi, " Porsi Mie Ayam = Rp", totalmkn)
+        mkn = ("Mie Ayam")
+    else:
+        print("Pilihan tidak ada, silahkan masukan lagi!!")
+        fungsimakanan()
+
+
+def fungsiminuman():
+    global totalmnm
+    global mnm
+    global gelas
+    print("\n----------------- Menu Minuman -----------------")
+    print("1. Es teh - Rp 2000")
+    print("2. Es jeruk - Rp 3500")
+    print("3. Es kopi - Rp 4000")
+    nomor = int(input("Masukan Pilihan: "))
+    gelas = int(input("Berapa Gelas ?: "))
+
+    if nomor == 1:
+        totalmnm = gelas * 2000
+        print(gelas, " Es Teh = Rp", totalmnm)
+        mnm = (" Gelas Es Teh")
+    elif nomor == 2:
+        totalmnm = gelas * 3500
+        print(gelas, " Gelas Es Jeruk = Rp", totalmnm)
+        mnm = ("Es Jeruk")
+    elif nomor == 3:
+        totalmnm = gelas * 4000
+        print(gelas, " Gelas Es Kopi = Rp", totalmnm)
+        mnm = ("Es Kopi")
+    else:
+        print("Pilihan tidak ada, silahkan masukan lagi!!")
+        fungsiminuman()
+
+
+fungsimakanan()
+fungsiminuman()
+totalsemua = totalmkn + totalmnm
+
+print("\nTotal harus Dibayar: Rp", totalsemua)
+uang = int(input("Uang Tunai Pembeli: Rp "))
+kembalian = int(uang - totalsemua)
+print("Kembalian :", kembalian)
+
+print("\n==========================================")
+print("============ STRUK PEMBELIAN =============")
+print("==========================================")
+print("Nama\t\t:", pembeli)
+print("Beli\t\t:", porsi, mkn, "( Rp", totalmkn, ")")
+print("\t\t ", gelas, mnm, "( Rp", totalmnm, ")")
+print("Tagihan\t\t: Rp", totalsemua)
+print("Dibayar\t\t: Rp", uang)
+print("Kembalian\t: Rp", kembalian)
+print("==========================================")
+print("       Terimakasih Sudah Berkunjung \n    ")
+print("            Selamat Menikmati             ")
+print("==========================================")
